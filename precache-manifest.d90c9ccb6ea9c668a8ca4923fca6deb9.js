30a9479549b4045b5ae81012d19f903530831753
MuhammadZubair <mzhassan444@gmail.com>
2020-10-12 13:57:47 +0500

    Updates

---
+++ precache-manifest.d90c9ccb6ea9c668a8ca4923fca6deb9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc7b3b42ecd17657d7c0a1140313317",
+    "url": "/Main-page/index.html"
+  },
+  {
+    "revision": "9f04bb70aa42af653548",
+    "url": "/Main-page/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "bb1b08163618859192e9",
+    "url": "/Main-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9f04bb70aa42af653548",
+    "url": "/Main-page/static/js/2.8a6c54a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Main-page/static/js/2.8a6c54a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb1b08163618859192e9",
+    "url": "/Main-page/static/js/main.e8e01bb5.chunk.js"
+  },
+  {
+    "revision": "dab9b9d2017e19815090",
+    "url": "/Main-page/static/js/runtime-main.0906c890.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Main-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
